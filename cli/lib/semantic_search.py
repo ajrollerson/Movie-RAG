2f9dc9b545cead47a4ae21d250a64a8ec1307066
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-04-29 17:08:15 +0700

    Implement semantic search

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,7 +1,9 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import os
+import sys
 from .search_utils import load_movies
+from .constants import LIMIT
 
 def verify_model():
     semantic_search = SemanticSearch()
@@ -28,6 +30,16 @@ def embed_query_text(query):
     print(f"Query: {query}")
     print(f"First 3 dimensions: {embedding[:3]}")
     print(f"Shape: {embedding.shape}")
+
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+
+    return dot_product / (norm1 * norm2)
 
 class SemanticSearch:
     def __init__(self):
@@ -62,3 +74,17 @@ class SemanticSearch:
                 return self.embeddings
             return self.build_embeddings(documents)
         return self.build_embeddings(documents)
+        
+    def search(self, query, limit=LIMIT):
+        if self.embeddings is None:
+            raise ValueError("No embeddings loaded. Call `load_or_create_embeddings` first.")
+        query_embedding = self.generate_embedding(query)
+        scores = []
+        for i, document in enumerate(self.embeddings):
+            scores.append((cosine_similarity(query_embedding, self.embeddings[i]), self.documents[i]))
+        sorted_scores = sorted(scores, key=lambda x: x[0], reverse=True)
+        sliced_sorted_scores = sorted_scores[:limit] 
+        results = []
+        for score, doc in sliced_sorted_scores:
+            results.append({"score": score, "title": doc["title"], "description": doc["description"]})
+        return results
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model, embed_text, verify_embeddings, embed_query_text
-
+from lib.semantic_search import verify_model, embed_text, verify_embeddings, embed_query_text, SemanticSearch
+from lib.search_utils import load_movies
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -15,6 +15,10 @@ def main():
 
     embed_query_parser = subparsers.add_parser("embed_query", help="Embeds the query")
     embed_query_parser.add_argument("query", type=str, help="Query to be embedded")
+
+    search_parser = subparsers.add_parser("search", help="Searchs for semantically related movies")
+    search_parser.add_argument("query", type=str, help="Search query")
+    search_parser.add_argument("--limit", type=int, default=5)
 
     args = parser.parse_args()
 
@@ -30,6 +34,12 @@ def main():
 
         case "embed_query":
             embed_query_text(args.query)
+
+        case "search":
+            semantic_search = SemanticSearch()
+            semantic_search.load_or_create_embeddings(load_movies())
+            for i, result in enumerate(semantic_search.search(args.query, args.limit), start=1):
+                print(f"{i}. {result['title']} (score: {result['score']:.4f})\n{result['description'][:100]}")
 
         case _:
             parser.print_help()
