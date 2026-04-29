b91bb8d01ea022e3435fc2f8424142e0adf4a4cd
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-04-29 16:06:39 +0700

    Add query embeddings command

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model, embed_text, verify_embeddings
+from lib.semantic_search import verify_model, embed_text, verify_embeddings, embed_query_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     verify_parser = subparsers.add_parser("verify", help="Verify the embedding model")
     verify_embeddings_parser = subparsers.add_parser("verify_embeddings", help="Verify the embeddings")
+    
 
     embed_text_parser = subparsers.add_parser("embed_text", help="Embed the input text")
     embed_text_parser.add_argument("text", type=str, help="Text to be embedded")
+
+    embed_query_parser = subparsers.add_parser("embed_query", help="Embeds the query")
+    embed_query_parser.add_argument("query", type=str, help="Query to be embedded")
 
     args = parser.parse_args()
 
@@ -23,6 +27,9 @@ def main():
 
         case "embed_text":
             embed_text(args.text)
+
+        case "embed_query":
+            embed_query_text(args.query)
 
         case _:
             parser.print_help()
